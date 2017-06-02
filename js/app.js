a4eb39532616b5958b6992d505b9b1f4f20d25ba
h3xar0n <aron@glambyadelina.com>
2017-06-01 22:17:29 -0400

    Added love

--- js/app.js
+++ js/app.js
@@ -1,17 +1,24 @@
 var material = [
       { title: 'Desire', 
         subtitle: 'cupiditas',
-        descriptions: ['Desire is our very essence insofar as our essence is concieved as determined to any action.', 
+        descriptions: ['Desire is the actual essence of man, in so far as it is conceived, as determined to a particular activity by some given modification of itself.', 
           'Sometimes people do things which conduce to their perseverance and other times people do things which fail to so conduce. In both types of case, though, people desire to persevere. When I do something that fails to help me to persevere, it is because the ideas on which I based my action were confused; that is, I thought I knew what would help me to persevere, but I was wrong. When I do something that does help me to persevere, though (unless I have simply been lucky in acting from an inadequate idea), it is because I acted on clear and distinct ideas or, in other words, genuine knowledge about what would help me to persevere.']
       }, 
       { title: 'Joy',
         subtitle: 'laetitia',
-        descriptions: ['Joy is our transition from a state of less perfection to a state of greater perfection',
+        descriptions: ['Joy is the transition of a man from a less to a greater perfection',
           'Joy is the passive transition of the mind to a state of greater perfection. When it relates to part of the body, it is pleasure (titillatio), and when it relates to the whole body, it is cheerfulness (hilaritas). Pleasure can be excessive insofar as it prevents other parts of the body from their actions, but cheerfulness cannot be excessive since it involves an increase in perfection throughout the whole body.'] 
       },
       { title: 'Love',
         subtitle: 'amor',
-        descriptions: [''] },
+        descriptions: ['Love is joy, accompanied by the idea of an external cause.',
+          'This definition explains sufficiently clearly the essence of love ; the definition given by those authors who say that love is the lover\'s wish to unite himself to the loved object expresses a property, but not the essence of love ; and, as such authors have not sufficiently discerned love\'s essence, they have been unable to acquire a true conception of its properties, accordingly their definition is on all hands admitted to be very obscure.']
+      },
+      { title: 'Sorrow',
+        subtitle: 'tristitia',
+        descriptions: ['Sorrow is the transition of a man from a greater to a less perfection.',
+          'Sorrow is the passive transition of the mind to a state of greater perfection. When it relates to part of the body, it is pain (dolor), and when it relates to the whole body, it is melancholy (melancholia). Pain can be excessive insofar as it prevents excessive pleasure and thus enables more other parts of the body to act, but melancholy is always bad since it involves an prevents action throughout the body.']
+      },
       { title: 'Sausages'}
       ];
 
@@ -40,28 +47,4 @@ var main = new Vue({
                           'test test test'],
     example: 1
   }
-});
-
-
-// jQuery for Menu
-
-$(document).ready(function(){
-
-  // Menu Toggle
-  $(".menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-  });
-
-  // Icon Toggle
-  $('#nav-icon').click(function(){
-    $(this).toggleClass('open');
-  });
-  $('.menu-item a').on('click', function(){
-    i = $(this).attr('id');
-    console.log(i);
-    var title = $(this).html();
-    $('#focus-title').html(title);
-  });
-
 });

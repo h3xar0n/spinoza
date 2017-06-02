729efe0058373be0991314ebb40adca562fcf9ff
h3xar0n <aron@glambyadelina.com>
2017-06-01 22:04:58 -0400

    Adding content to emotions

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 var material = [
       { title: 'Desire', 
-        subtitle: 'What what',
-        descriptions: ['Lol I\'m loco', 'What the fuck']
-        }, 
-      { title: 'Hatred' },
-      { title: 'Joy' },
+        subtitle: 'cupiditas',
+        descriptions: ['Desire is our very essence insofar as our essence is concieved as determined to any action.', 
+          'Sometimes people do things which conduce to their perseverance and other times people do things which fail to so conduce. In both types of case, though, people desire to persevere. When I do something that fails to help me to persevere, it is because the ideas on which I based my action were confused; that is, I thought I knew what would help me to persevere, but I was wrong. When I do something that does help me to persevere, though (unless I have simply been lucky in acting from an inadequate idea), it is because I acted on clear and distinct ideas or, in other words, genuine knowledge about what would help me to persevere.']
+      }, 
+      { title: 'Joy',
+        subtitle: 'laetitia',
+        descriptions: ['Joy is our transition from a state of less perfection to a state of greater perfection',
+          'Joy is the passive transition of the mind to a state of greater perfection. When it relates to part of the body, it is pleasure (titillatio), and when it relates to the whole body, it is cheerfulness (hilaritas). Pleasure can be excessive insofar as it prevents other parts of the body from their actions, but cheerfulness cannot be excessive since it involves an increase in perfection throughout the whole body.'] 
+      },
+      { title: 'Love',
+        subtitle: 'amor',
+        descriptions: [''] },
       { title: 'Sausages'}
       ];
 
@@ -28,7 +35,7 @@ var main = new Vue({
   data: {
     title:       'Ethics',
     subtitle:    'in the Programattic Order',
-    descriptions: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    descriptions: ['',
                           'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                           'test test test'],
     example: 1
